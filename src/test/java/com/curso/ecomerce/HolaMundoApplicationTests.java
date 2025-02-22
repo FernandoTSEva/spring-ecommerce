f77d47ef61f9d180498c2849eadfca408969c16f
FernandoTSEva <fer.e.t.sanchez@gmail.com>
2025-02-22 11:05:44 -0600

    creación de la estructura del proyecto y la clase model

---
+++ src/test/java/com/curso/ecomerce/HolaMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
