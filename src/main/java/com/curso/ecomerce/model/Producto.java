411415b94917b6637fc5c2ca68b97dd8d8165fe8
FernandoTSEva <fer.e.t.sanchez@gmail.com>
2025-03-05 13:27:29 -0600

    crear las clases model, mapeo clases a tablas con JPA, integrando la vista y diseño

--- src/main/java/com/curso/ecomerce/model/Producto.java
+++ src/main/java/com/curso/ecomerce/model/Producto.java
@@ -1,7 +1,19 @@
 package com.curso.ecomerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
+
 public class Producto {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
@@ -9,12 +21,15 @@ public class Producto {
 	private double precio;
 	private int cantidad;
 	
+	@ManyToOne
+	private Usuario usuario;
+	
 	public Producto() {
 		// TODO Auto-generated constructor stub
 	}
-	
-
-	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad) {
+		
+	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad,
+			Usuario usuario) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
@@ -22,8 +37,12 @@ public class Producto {
 		this.imagen = imagen;
 		this.precio = precio;
 		this.cantidad = cantidad;
+		this.usuario = usuario;
 	}
-	
+
+
+
+
 
 	public Integer getId() {
 		return id;
@@ -71,6 +90,15 @@ public class Producto {
 
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 
 
--- src/main/java/com/curso/ecomerce/model/Usuario.java
+++ src/main/java/com/curso/ecomerce/model/Usuario.java
@@ -1,6 +1,21 @@
 package com.curso.ecomerce.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+
 public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String username;
@@ -8,6 +23,13 @@ public class Usuario {
 	private String direccion;
 	private String tipo;
 	private String password;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Producto>productos;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Orden> ordenes;
+	
 	
 	public Usuario() {
 		// TODO Auto-generated constructor stub
@@ -80,7 +102,21 @@ public class Usuario {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	
-	
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", username=" + username + ", email=" + email
+				+ ", direccion=" + direccion + ", tipo=" + tipo + ", password=" + password + ", productos=" + productos
+				+ "]";
+	}
 	
 }
