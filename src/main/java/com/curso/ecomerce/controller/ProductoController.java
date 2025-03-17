69b46b09a92df18c51619f9b1a47a8b1dcaffbbc
FernandoTSEva <fer.e.t.sanchez@gmail.com>
2025-03-17 16:29:10 -0600

    implementacion para guardar la imagen del producto

--- src/main/java/com/curso/ecomerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecomerce/controller/ProductoController.java
@@ -1,6 +1,7 @@
 package com.curso.ecomerce.controller;
 
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.*;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.curso.ecomerce.model.Producto;
 import com.curso.ecomerce.model.Usuario;
 import com.curso.ecomerce.service.ProductoService;
+import com.curso.ecomerce.service.UploadFileService;
 
 @Controller
 @RequestMapping("/productos")
@@ -24,6 +28,9 @@ public class ProductoController {
 	
 	@Autowired
 	private ProductoService productoService;
+	
+	@Autowired
+	private  UploadFileService upload;
 	
 	@GetMapping("")
 	public String show(Model model){
@@ -37,10 +44,26 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/save")
-	public String save(Producto producto) {
+	public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
 		LOGGER.info("Este es un objeto producto {}", producto);
 		Usuario u= new Usuario(1, "", "", "", "", "", "");
 		producto.setUsuario(u);
+		
+		//imagen
+		if(producto.getId()==null) {
+			String nombreImagen= upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}else {
+			if(file.isEmpty()) {
+				Producto p= new Producto();
+				p=productoService.get(producto.getId()).get();
+				producto.setImagen(p.getImagen());
+			}else {
+				String nombreImagen= upload.saveImage(file);
+				producto.setImagen(nombreImagen);
+			}
+		}
+		
 		productoService.save(producto);
 		return "redirect:/productos";
 	}
